a02a0e5b2a8313f7f1e801e65ea01defe6efdf26
salman <salmanhoney987@gmail.com>
2024-08-23 10:50:04 +0500

    feat: Onboarding user interface

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import PatientForm from "@/components/forms/PatientForm";
 import { Button } from "@/components/ui/button";
+import { Link } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="flex h-screen max-h-screen">
+      {/* OTP verifcation Modal */}
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <div className="flex gap-4">
@@ -16,8 +19,26 @@ export default function Home() {
             />
             <div className="text-2xl">Proton</div>
           </div>
+
+          <PatientForm />
+
+          <div className="text-14-regular flex justify-between">
+            <p className="justify-items-end text-dark-600 xl: text-left">
+              ProtonApp Powered by Next LinkedIn :
+              https://www.linkedin.com/in/salman-mughal-full-stack-developer/
+            </p>
+            <Link href="/?admin=true">Admin</Link>
+          </div>
         </div>
       </section>
+
+      <Image
+        height={1000}
+        width={1000}
+        src={"/assets/onboarding.png"}
+        alt="Onboarding Screen...."
+        className="side-img max-w-[30%]"
+      />
     </div>
   );
 }
