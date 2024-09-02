91cc463b3f908e688bd3dd9960a83031ba7d65fd
salman <salmanhoney987@gmail.com>
2024-09-02 11:43:37 +0500

    feat: OTP Modal

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,15 @@ import PatientForm from "@/components/forms/PatientForm";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import Image from "next/image";
+import { SearchParamProps } from "@/interfaces";
+import { OTPAdmin } from "@/components/otp/OTPAdmin";
 
-export default function Home() {
+export default function Home({ searchParams }: SearchParamProps) {
+  const isAdmin = searchParams.admin === "true";
   return (
     <div className="flex h-screen max-h-screen">
       {/* OTP verifcation Modal */}
+      {isAdmin && <OTPAdmin />}
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <div className="flex gap-4">
